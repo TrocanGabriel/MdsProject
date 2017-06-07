9c442a0e8939cb5b98b44a04d1c0fb3dad0588ee
1Mihail <gabriel714@yahoo.com>
2017-06-07 17:26:44 +0300

    improved design of contact

--- cinemapedia/index-contact.php
+++ cinemapedia/index-contact.php
@@ -28,12 +28,12 @@
   <div class="form">
     <div style="font-family:Arial; font-size:16px; color:#ef1f2f; font-weight:bold;">
     <form  method="post" action="acknowledge.php">
-    <label for="name">Name:</label>
-	<input type="text" name="name" id="name">
-    <label for="email">Email:</label>
-	<input type="text" name="email" id="email">
-    <label for="message">Message:</label>
-	<textarea name="message" id="message"></textarea>
+    <label for="name">Name:</label><br>
+	<input type="text" name="name" id="name"><br>
+    <label for="email">Email:</label><br>
+	<input type="text" name="email" id="email"><br>
+    <label for="message">Message:</label><br>
+	<textarea name="message" id="message"></textarea><br>
    <input type="submit" name="submit" value="Submit">
    </form>
   </div>
